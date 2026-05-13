4d56287f78dd1a14c67b7327965dfe82539d1b69
selvaratnamsonia <selvaratnamsonia@gmail.com>
2026-05-13 22:20:21 +0800

    Firebase configuration file added to the project. This file contains the necessary configuration details to connect the application to the Firebase backend services, enabling features such as authentication, database interactions, and storage. The inclusion of this file is essential for the proper functioning of the application's Firebase integration.

---
+++ js/firebase-config.js
@@ -0,0 +1,19 @@
+// js/firebase-config.js (by Sonia Selvaratnam 13/5/2026)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVIQytNF0kQ5ZZh1J8Uc56pHhoL1bG90M",
+  authDomain: "utm-clinic-portal.firebaseapp.com",
+  projectId: "utm-clinic-portal",
+  storageBucket: "utm-clinic-portal.firebasestorage.app",
+  messagingSenderId: "579361367162",
+  appId: "1:579361367162:web:4e45c841ec63756aff07c3",
+  measurementId: "G-W1LNRL2VVY"   // optional, you can keep it or remove it
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+// Sonia Selvaratnam 13/5/2026
